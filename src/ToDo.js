f5cee526a6c7bfb65cf40f25d71762baac0fc431
Sergey-Ivanovich <ericnoata@gmail.com>
2024-04-19 11:08:03 +0100

    contined implementing search and add feature

--- src/ToDo.js
+++ src/ToDo.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 export default function ToDo() {
   const [searchBar, SetSearchBar] = useState("");
-  const [toDoItems, setToDoItems] = useState([""]);
+  const [toDoItems, setToDoItems] = useState(["hello"]);
 
   function refreshSearchBar(event) {
     SetSearchBar(event.target.value);
@@ -11,34 +11,57 @@ export default function ToDo() {
 
   function enterSearch(event) {
     event.preventDefault();
-    let itemMemory = toDoItems.concat(searchBar);
-    console.log(itemMemory);
-    setToDoItems(itemMemory);
+    if (searchBar.length !== 0) {
+      let itemMemory = toDoItems.concat(searchBar);
+      console.log(itemMemory);
+      setToDoItems(itemMemory);
+    }
   }
 
-  return (
-    <div className="ToDo container">
-      <div className="w-50 d-block m-auto">
-        <form className="text-center mt-4 mb-2 d-flex">
-          <input
-            type="text"
-            onChange={refreshSearchBar}
-            className="btn border border-gray text-start w-75"
-          />
-          <input
-            type="Submit"
-            className="btn btn-primary w-25"
-            onClick={enterSearch}
-          />
-        </form>
-        <div className="border border-gray w-100 rounded p-3">
-          <ul>
-            {toDoItems.map(function (oneItem) {
-              return <li>{oneItem}</li>;
-            })}
-          </ul>
+  if (toDoItems !== null) {
+    return (
+      <div className="ToDo container">
+        <div className="w-50 d-block m-auto">
+          <form className="text-center mt-4 mb-2 d-flex">
+            <input
+              type="text"
+              onChange={refreshSearchBar}
+              className="btn border border-gray text-start w-75"
+            />
+            <input
+              type="Submit"
+              className="btn btn-primary w-25"
+              onClick={enterSearch}
+            />
+          </form>
+          <div className="border border-gray w-100 rounded p-1 pt-3">
+            <ul>
+              {toDoItems.map(function (oneItem, index) {
+                return <li key={index}>{oneItem}</li>;
+              })}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="ToDo container">
+        <div className="w-50 d-block m-auto">
+          <form className="text-center mt-4 mb-2 d-flex">
+            <input
+              type="text"
+              onChange={refreshSearchBar}
+              className="btn border border-gray text-start w-75"
+            />
+            <input
+              type="Submit"
+              className="btn btn-primary w-25"
+              onClick={enterSearch}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
